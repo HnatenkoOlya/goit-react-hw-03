69c4d02eb3bf310ca27124ca071647421aec26fa
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-04-02 19:39:06 +0200

    3/1

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+//import ContactForm from './contactform/ContactForm.jsx'
+import './App.css'
+
+export default function App () {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+       
+   </div>
+
+  )
+}
